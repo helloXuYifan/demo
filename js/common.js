3f518830e7df85888d8e13d56b6a20de12238128
XuYifan <343882323@qq.com>
2022-11-23 16:32:45 +0800

    注册登录功能

--- js/common.js
+++ js/common.js
@@ -5,3 +5,44 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.classList.add('ready')
   }, 200)
 })
+
+// 配置请求的基准URL地址
+axios.defaults.baseURL = 'http://ajax-api.itheima.net'
+
+// 封装 轻提示
+function toast(str) {
+  document.querySelector('.toast-body').innerHTML = str
+  const toast = new bootstrap.Toast(document.getElementById('myToast'))
+  toast.show()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
